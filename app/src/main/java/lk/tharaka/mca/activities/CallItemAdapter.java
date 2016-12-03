60c13fc8396c7d569adb145912a9507362c67626
Tharaka Devinda <tdevinda@gmail.com>
2016-12-04 01:01:05 +0530

    listing ok, calling ok. Settings can be changed

--- app/src/main/java/lk/tharaka/mca/activities/CallItemAdapter.java
+++ app/src/main/java/lk/tharaka/mca/activities/CallItemAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import lk.tharaka.mca.MCAPreference;
 import lk.tharaka.mca.MissedCall;
 import lk.tharaka.mca.R;
 
@@ -23,8 +24,15 @@ public class CallItemAdapter extends ArrayAdapter<MissedCall> {
     private int resourceID;
     private List<MissedCall> itemList;
 
+    private MCAPreference preference;
+
     public CallItemAdapter(Context context, int resource, List<MissedCall> objects) {
         super(context, resource, objects);
+        this.context = context;
+        this.resourceID = resource;
+        this.itemList = objects;
+
+        preference = new MCAPreference(context);
     }
 
 
@@ -36,9 +44,24 @@ public class CallItemAdapter extends ArrayAdapter<MissedCall> {
 
         View item = inflater.inflate(R.layout.item_missedcall, null);
         TextView nameTextView = (TextView) item.findViewById(R.id.text_item_callername);
-        nameTextView.setText("Test");
+        TextView callCounterTextView = (TextView) item.findViewById(R.id.text_item_count_counter);
+        TextView callCounterLabelTextView = (TextView) item.findViewById(R.id.text_item_count_label);
+        TextView timestampTextView = (TextView) item.findViewById(R.id.text_item_date);
 
+        int callCount = itemList.get(position).count;
 
+        nameTextView.setText(itemList.get(position).name);
+        callCounterTextView.setText(!preference.isIncludeNumbersInSMS()?
+                callCount + "":
+                context.getResources().getStringArray(R.array.messageCounter)[callCount - 1]);
+
+        callCounterLabelTextView.setText((itemList.get(position).count > 1)?
+                context.getString(R.string.messageListItemCallLabelMultiple):
+                context.getString(R.string.messageListItemCallLabelSingle));
+        timestampTextView.setText(itemList.get(position).date);
         return item;
+
     }
+
+
 }
--- app/src/main/java/lk/tharaka/mca/activities/CallListActivity.java
+++ app/src/main/java/lk/tharaka/mca/activities/CallListActivity.java
@@ -1,5 +1,6 @@
 package lk.tharaka.mca.activities;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -8,6 +9,11 @@ import android.service.carrier.CarrierMessagingService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lk.tharaka.mca.MCACommon;
+import lk.tharaka.mca.MCAPreference;
 import lk.tharaka.mca.MissedCall;
 import lk.tharaka.mca.R;
 
@@ -25,67 +32,101 @@ public class CallListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_list);
 
-        Log.d("CALLL", "starting to read sms");
-        System.out.println("starting...");
-        /*SMSTask task = new SMSTask();
-        task.execute();*/
-
-        List<MissedCall> missedCalls = getMissedCallsFromSMS();
-        CallItemAdapter adapter = new CallItemAdapter(
-                getApplicationContext(),
-                R.layout.item_missedcall,
-                missedCalls
-        );
-
-        ListView lv = (ListView) findViewById(R.id.listView_callList);
-        lv.setAdapter(adapter);
-
+        SMSTask task = new SMSTask();
+        task.execute();
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(getApplicationContext());
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_settings) {
+            Intent showSettingsIntent = new Intent(getApplicationContext(), MCASettingsActivity.class);
+            startActivity(showSettingsIntent);
+        }
+
+        return true;
+
+    }
 
     private List<MissedCall> getMissedCallsFromSMS() {
         List<MissedCall> missedCalls = new ArrayList<>();
         MCACommon common = new MCACommon(getApplicationContext());
 
-        Log.i("SMS", "starting to read");
         Cursor smsCursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, "address=?", new String[] {"Alert"}, null);
 
         smsCursor.moveToFirst();
 
         while (!smsCursor.isAfterLast()) {
-            Log.i("CALLL", "====");
-            for (int i = 0; i < smsCursor.getColumnCount(); i++) {
-                Log.i("CALLL", String.format("%s: %s", smsCursor.getColumnName(i), smsCursor.getString(i)));
-                List<MissedCall> currentSMSCalls = common.extractMissedCallsFromSMS(
-                        smsCursor.getString(smsCursor.getColumnIndex("body")),
-                        smsCursor.getString(smsCursor.getColumnIndex("address")),
-                        smsCursor.getString(smsCursor.getColumnIndex("service_center")));
+            List<MissedCall> currentSMSCalls = common.extractMissedCallsFromSMS(
+                    smsCursor.getString(smsCursor.getColumnIndex("body")),
+                    smsCursor.getString(smsCursor.getColumnIndex("address")),
+                    smsCursor.getString(smsCursor.getColumnIndex("service_center")));
 
-                missedCalls.addAll(currentSMSCalls);
-            }
-
+            missedCalls.addAll(currentSMSCalls);
             smsCursor.moveToNext();
-
         }
 
         return  missedCalls;
 
     }
 
-    private class SMSTask extends AsyncTask<Void, Void, Void> {
+    private void callNumber(String number) {
+        Intent callPhoneIntent = new Intent(getApplicationContext(), NotificationActionActivity.class);
+        callPhoneIntent.putExtra(MCACommon.EXTRA_NEXT_ACTION, MCACommon.ACTION_CALL_PHONE);
+        callPhoneIntent.putExtra(MCACommon.EXTRA_PHONE_NUMBER, number);
+
+        startActivity(callPhoneIntent);
+        finish();
+
+    }
+
+    private class SMSTask extends AsyncTask<Void, Void, List<MissedCall>> {
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            Toast.makeText(getApplicationContext(), "Starting fetch", Toast.LENGTH_SHORT).show();
+
+            Toast.makeText(getApplicationContext(), "Fetching data...", Toast.LENGTH_SHORT).show();
         }
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            getMissedCallsFromSMS();
-            return null;
+        protected List<MissedCall> doInBackground(Void... voids) {
+            MCACommon common = new MCACommon(getApplicationContext());
+
+            List<MissedCall> missedCalls = getMissedCallsFromSMS();
+            for (MissedCall call : missedCalls) {
+                call.name = common.getNameFor(call.number);
+            }
+            return missedCalls;
+        }
+
+        @Override
+        protected void onPostExecute(List<MissedCall> missedCalls) {
+            super.onPostExecute(missedCalls);
+
+            CallItemAdapter adapter = new CallItemAdapter(
+                    getApplicationContext(),
+                    R.layout.item_missedcall,
+                    missedCalls
+            );
+
+
+            final ListView lv = (ListView) findViewById(R.id.listView_callList);
+            lv.setAdapter(adapter);
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    callNumber(((MissedCall) lv.getItemAtPosition(i)).number);
+
+                }
+            });
         }
     }
 
